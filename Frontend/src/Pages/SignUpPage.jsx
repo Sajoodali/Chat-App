fc9cc369bd9643f80c84d84f1248a7789d2e0d81
sajood ali <sajoodali486@gmail.com>
2025-10-03 03:35:53 +0500

    changes

--- Frontend/src/Pages/SignUpPage.jsx
+++ Frontend/src/Pages/SignUpPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import useAuthStore from "../store/useAuthStore";
+import { MessageSquare } from "lucide-react";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -19,7 +20,35 @@ const SignUpPage = () => {
     e.preventDefault();
   };
 
-  return <div>SignUpPage</div>;
+  return (
+    <>
+      <div className="min-h-screen grid lg:grid-cols-2">
+        {/* left side */}
+        <div className="flex flex-col justify-center p-6 sm:p-12">
+          <div className="w-full max-w-md space-y-8">
+            {/* logo */}
+            <div className="text-center mb-8">
+              <div className="flex flex-col items-center gap-2 group">
+                <div
+                  className="size-12 rounded-xl bg-primary/10 flex items-center justify-center
+                group-hover:bg-primary/20 transition-all duration-300"
+                >
+                  <MessageSquare className="size-6 text-primary" />
+                </div>
+                <h1 className="text-2xl font-bold mt-2">Create an account</h1>
+                <p className="text-base-content/70">
+                  Get Started With Your Free Account
+                </p>
+              </div>
+            </div>
+
+            {/* form */}
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignUpPage;
